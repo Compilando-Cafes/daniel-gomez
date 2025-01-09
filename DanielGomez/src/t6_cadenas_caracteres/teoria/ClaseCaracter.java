cb8e246c8fc679080312f17f53c74eceeb526e2a
Daniel Gómez Delgado <danigom11@gmail.com>
2025-01-09 11:55:02 +0100

    guardo ejercicios cadenas

---
+++ DanielGomez/src/t6_cadenas_caracteres/teoria/ClaseCaracter.java
@@ -0,0 +1,70 @@
+package t6_cadenas_caracteres.teoria;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ClaseCaracter {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        char[] listaCaracteresUsuario = new char[15];
+        int tamanoListaNumeros = 0;
+        char[] listaNumeros = new char[tamanoListaNumeros];
+        int tamanoListaLetras = 0;
+        char[] listaLetras = new char[tamanoListaLetras];
+        int tamanoListaNumerosYLetras = 0;
+        char[] listaNumerosYLetras = new char[tamanoListaNumerosYLetras];
+        int tamanoListaEspaciosVacios = 0;
+        char[] listaEspaciosVacios = new char[tamanoListaEspaciosVacios];
+
+        for (int i = 0; i < 15; i++) {
+            System.out.print("Carácter número " + (i+1) + ": ");
+            char caracter = sc.next().charAt(0);
+            listaCaracteresUsuario[i] = caracter;
+
+            // Comprobar si es número y añadirlo
+            if (Character.isDigit(caracter)){
+                tamanoListaNumeros++;
+                listaNumeros = Arrays.copyOf(listaNumeros, tamanoListaNumeros);
+                listaNumeros[tamanoListaNumeros-1] = caracter;
+            }
+
+            // Comprobar si es letra y añadirlo
+            if (Character.isLetter(caracter)){
+                tamanoListaLetras++;
+                listaLetras = Arrays.copyOf(listaLetras, tamanoListaLetras);
+                listaLetras[tamanoListaLetras-1] = caracter;
+            }
+
+            // Comprobar si es número o letra y añadirlo
+            if (Character.isLetterOrDigit(caracter)){
+                tamanoListaNumerosYLetras++;
+                listaNumerosYLetras = Arrays.copyOf(listaNumerosYLetras, tamanoListaNumerosYLetras);
+                listaNumerosYLetras[tamanoListaNumerosYLetras-1] = caracter;
+            }
+
+            // Comprobar si es espacio vacío y añadirlo
+            if (Character.isWhitespace(caracter)){
+                tamanoListaEspaciosVacios++;
+                listaEspaciosVacios = Arrays.copyOf(listaEspaciosVacios, tamanoListaEspaciosVacios);
+                listaEspaciosVacios[tamanoListaEspaciosVacios-1] = caracter;
+            }
+        }
+
+        System.out.println("Lista de caracteres introducidos por el usuario:");
+        System.out.println(Arrays.toString(listaCaracteresUsuario));
+
+        System.out.println("Lista de números:");
+        System.out.println(Arrays.toString(listaNumeros));
+
+        System.out.println("Lista de letras:");
+        System.out.println(Arrays.toString(listaLetras));
+
+        System.out.println("Lista de números y letras:");
+        System.out.println(Arrays.toString(listaNumerosYLetras));
+
+        System.out.println("Lista de espacios vacíos:");
+        System.out.println(Arrays.toString(listaEspaciosVacios));
+    }
+
+
+}
---
+++ DanielGomez/src/t6_cadenas_caracteres/teoria/Unicode.java
@@ -0,0 +1,28 @@
+package t6_cadenas_caracteres.teoria;
+
+public class Unicode {
+    public static void main(String[] args) {
+
+        int a = 'a';
+        int b = '\u0061';
+        char c = 97;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println((char) ('a' + 1));
+
+
+        System.out.println((int)'e' + " " + (int) 'A' + " " + (int) 'a');
+        int letraMayuscula = (int)'e' +  (int)'A' - (int)'a';
+        System.out.println((char)letraMayuscula);
+        System.out.println((int)'E');
+
+        // Almacenar un hexadecimal
+        int codePoint = 0xFFFF;
+
+        for (int i =0x0000; i<=0xFFFF;i++) {
+            System.out.println(Integer.toHexString(i) + " " + (char)i);
+        }
+
+    }
+}
